5c0fd40ab775430e17b2dac5de40bcdbd3b6fc00
Reza <development.rezasajadi@gmail.com>
2021-07-27 13:32:23 +0430

    Adding slider to search form.

--- src/containers/Influencer/List/SearchForm.js
+++ src/containers/Influencer/List/SearchForm.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import { Button } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 import { getInfluencers } from "../../../store/slice/influencerSlice";
 import settings from "../../../settings.json";
+import CustomSlider from "../../../components/Slider/CustomSlider";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,14 +23,18 @@ const SearchForm = () => {
   const [inputValue, setInputValue] = useState();
   const dispatch = useDispatch();
 
+  const sliderRef = useRef();
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    const sliderValues = sliderRef.current.getValues();
+
     dispatch(
       getInfluencers({
         text: inputValue,
         followers: {
-          min_count: 0,
-          max_count: 0,
+          min_count: sliderValues.min,
+          max_count: sliderValues.max,
         },
       })
     );
@@ -49,6 +54,8 @@ const SearchForm = () => {
         label="Text Field"
         onChange={handleInputChange}
       />
+
+      <CustomSlider ref={sliderRef} />
 
       <Button variant="contained" color="primary" size="small" type="submit">
         {settings.searchForm.submitButtonText}
