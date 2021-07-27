515fe0546939bbd550a4db0d7fa84a513636ce33
Reza <development.rezasajadi@gmail.com>
2021-07-27 18:42:12 +0430

    Adding auth.

--- src/rest/ResiAPI.js
+++ src/rest/ResiAPI.js
@@ -2,7 +2,17 @@ import Axios from "axios";
 
 class RestAPI {
   SendRequest = (method, url, params, data, headers) =>
-    Axios.request({ method, url, params, data, headers });
+    Axios.request({
+      method,
+      url,
+      params,
+      data,
+      auth: {
+        username: "sponsokit_search_example",
+        password: "10c3c28026f540b480a9a0896f04ebee",
+      },
+      headers,
+    });
 }
 
 export default RestAPI;
