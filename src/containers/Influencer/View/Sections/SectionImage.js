673a0e3ceb20a5aa7f5868fe43bf382b830c42a0
Reza <development.rezasajadi@gmail.com>
2021-07-29 00:47:22 +0430

    Handling the situation when the influencer profile not found.

--- src/containers/Influencer/View/Sections/SectionImage.js
+++ src/containers/Influencer/View/Sections/SectionImage.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/styles";
 import { CardMedia } from "@material-ui/core";
+import notFound from "../../../../assets/images/profile-not-found-image.jpg";
 
 const useStyles = makeStyles((theme) => ({
   cover: {
@@ -16,10 +17,19 @@ const useStyles = makeStyles((theme) => ({
   img: {
     width: 160,
   },
+  fallbackImg: {
+    width: 160,
+    objectFit: "cover",
+  },
 }));
 
 const SectionImage = ({ mediaURL }) => {
   const classes = useStyles();
+
+  const handleImageError = (event) => {
+    event.target.className = classes.fallbackImg;
+    event.target.src = notFound;
+  };
 
   return (
     <div className={classes.cover}>
@@ -29,6 +39,7 @@ const SectionImage = ({ mediaURL }) => {
         className={classes.img}
         image={mediaURL}
         title="Influencer Profile Image"
+        onError={handleImageError}
       />
     </div>
   );
